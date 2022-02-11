3821d69d9c97083b6407ec69adfe5c37ec2901b9
David Tejuosho <tejuoshodavid@gmail.com>
2022-02-11 08:02:57 -0600

    Replaced unnecessary list with array. Replaced thrown exception with stacktrace printing. optimized imports

--- src/Scorer.java
+++ src/Scorer.java
@@ -2,10 +2,7 @@ import javax.swing.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Scorer {
 	private static final Object[][] SCORE_ARRAY = {
@@ -32,47 +29,55 @@ public class Scorer {
 		return totalScore;
 	}
 	
-	static int getLetterScore(char letter) throws Exception {
+	static int getLetterScore(char letter){
 		assert Character.isLetter(letter);
 		letter = Character.toUpperCase(letter);
 		for (Object[] score : SCORE_ARRAY)
 			for (int i = 1; i < score.length; i++)
 				if ((char) score[i] == letter)
 					return (int) score[0];
-		throw new Exception("This isn't supposed to happen. Score array may be corrupted");
+		new Exception("This isn't supposed to happen. Score array may be corrupted").printStackTrace();
+		return -1;
 	}
 	
 	public static void checkHighScore(int score, Player player) throws FileNotFoundException {
-		Scanner leaderboard = new Scanner(new File("data-files\\leaderboard.txt"));
-		List<List<Object>> leaderboardList = new ArrayList<>();
+		Scanner leaderboardScanner = new Scanner(new File("data-files" + File.separatorChar + "leaderboard.txt"));
+		List<Object[]> leaderboardList = new ArrayList<>();
+//		List of array. Array contains name at index 0 and score at index 1
 		
 		//Load the array
 		for (int i = 0; i < 10; i++) {
-			leaderboardList.add(new ArrayList<>());
-			leaderboardList.get(i).add(leaderboard.next());
-			leaderboardList.get(i).add(Integer.parseInt(leaderboard.next()));
+			leaderboardList.add(
+					new Object[]{
+							leaderboardScanner.next()
+							, Integer.parseInt(leaderboardScanner.next())
+					}
+			);
 		}
 		
-		for (int i = 9; i >= 0; i--)
-			if ((int) leaderboardList.get(i).get(1) > score) {
+		for (int i = 10; i > 0; i--)
+			if ((int) leaderboardList.get(i - 1)[1] > score) {
 				//Magic fuckery that checks for the highest score that is lower than our score and inserts our score at
 				//that index by streaming an array of our name and score to a list and adding that using the List.add(int, T) method
 				//Will probably need to revise this to improve readability.
-				leaderboardList.add(i + 1, Arrays.stream(new Object[]{player.getNAME(), score}).toList());
-				if (i < 9) newHighScore(leaderboardList, i + 1);
+				leaderboardList.add(i, Arrays.stream(new Object[]{player.getNAME(), score}).toArray());
+				if (i - 1 < 9) newHighScore(leaderboardList, i);//if the
 				break;
 			}
 		
 		if (leaderboardList.size() == 10) {
 			//if true, this would mean our score is higher than all the scores on the leaderboard, so we would need to put it
 			//at the top using the same magic fuckery from before
-			leaderboardList.add(0, Arrays.stream(new Object[]{player.getNAME(), score}).toList());
+			leaderboardList.add(0
+					, Arrays.stream(new Object[]{player.getNAME(), score})
+							.toArray()
+			);
 			newHighScore(leaderboardList, 1);
 		}
 	}
 	
-	private static void newHighScore(List<List<Object>> leaderboard, int pos) throws FileNotFoundException {
-		var file = new File("data-files" + File.separator + "leaderboard.txt");
+	private static void newHighScore(List<Object[]> leaderboard, int pos) throws FileNotFoundException {
+		var file = new File("data-files" + File.separatorChar + "leaderboard.txt");
 		if (!file.exists()) {
 			var writer = new PrintWriter(file);
 			for (int i = 0; i < 10; i++)
@@ -82,26 +87,26 @@ public class Scorer {
 		var writer = new PrintWriter(file);
 		
 		for (int i = 0; i < 10; i++)
-			writer.printf("%s\t%d%n", leaderboard.get(i).get(0), (int) leaderboard.get(i).get(1));
+			writer.printf("%s\t%d%n", leaderboard.get(i)[0], (int) leaderboard.get(i)[1]);
 		writer.close();
 		
-		StringBuilder boardToShow = new StringBuilder(String.format("%-10s%-15s%s%n", "", "Name", "Score"));
+		StringBuilder boardToShow = new StringBuilder(String.format("%-15s%-15s%6s%n", "   ", "Name", "Score"));
 		
 		switch (pos) {
 			case 1, 2 -> {
-				boardToShow.append(String.format("%-10s%-15s%d%n", "1st", leaderboard.get(0).get(0), (int) leaderboard.get(0).get(1)));
-				boardToShow.append(String.format("%-10s%-15s%d%n", "2nd", leaderboard.get(1).get(0), (int) leaderboard.get(1).get(1)));
-				boardToShow.append(String.format("%-10s%-15s%d%n...", "3rd", leaderboard.get(2).get(0), (int) leaderboard.get(2).get(1)));
+				boardToShow.append(String.format("%-15s%-15s%6d%n", "1st", leaderboard.get(0)[0], (int) leaderboard.get(0)[1]));
+				boardToShow.append(String.format("%-15s%-15s%6d%n", "2nd", leaderboard.get(1)[0], (int) leaderboard.get(1)[1]));
+				boardToShow.append(String.format("%-15s%-15s%6d%n...", "3rd", leaderboard.get(2)[0], (int) leaderboard.get(2)[1]));
 			}
 			case 9, 10 -> {
-				boardToShow.append(String.format("...%n%-10s%-15s%d%n", "8th", leaderboard.get(7).get(0), (int) leaderboard.get(7).get(1)));
-				boardToShow.append(String.format("%-10s%-15s%d%n", "9th", leaderboard.get(8).get(0), (int) leaderboard.get(8).get(1)));
-				boardToShow.append(String.format("%-10s%-15s%d%n", "10th", leaderboard.get(9).get(0), (int) leaderboard.get(9).get(1)));
+				boardToShow.append(String.format("...%n%-15s%-15s%6d%n", "8th", leaderboard.get(7)[0], (int) leaderboard.get(7)[1]));
+				boardToShow.append(String.format("%-15s%-15s%6d%n", "9th", leaderboard.get(8)[0], (int) leaderboard.get(8)[1]));
+				boardToShow.append(String.format("%-15s%-15s%6d%n", "10th", leaderboard.get(9)[0], (int) leaderboard.get(9)[1]));
 			}
 			default -> {
-				boardToShow.append(String.format("...%n%-10s%-15s%d%n", getOrdinal(pos - 1), leaderboard.get(pos - 2).get(0), (int) leaderboard.get(pos - 2).get(1)));
-				boardToShow.append(String.format("%-10s%-15s%d%n", getOrdinal(pos), leaderboard.get(pos - 1).get(0), (int) leaderboard.get(pos - 1).get(1)));
-				boardToShow.append(String.format("%-10s%-15s%d%n...", getOrdinal(pos + 1), leaderboard.get(pos).get(0), (int) leaderboard.get(pos).get(1)));
+				boardToShow.append(String.format("...%n%-15s%-15s%6d%n", getOrdinal(pos - 1), leaderboard.get(pos - 2)[0], (int) leaderboard.get(pos - 2)[1]));
+				boardToShow.append(String.format("%-15s%-15s%6d%n", getOrdinal(pos), leaderboard.get(pos - 1)[0], (int) leaderboard.get(pos - 1)[1]));
+				boardToShow.append(String.format("%-15s%-15s%6d%n...", getOrdinal(pos + 1), leaderboard.get(pos)[0], (int) leaderboard.get(pos)[1]));
 			}
 		}
 		
